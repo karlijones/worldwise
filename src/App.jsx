37a2b1b3460c25c4e39092e55202b96c08179929
karlijones <karlijones1984@gmail.com>
2025-09-07 15:37:38 -0500

    Product page imported and Route created in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
+import Product from "./pages/Product";
 
 function App() {
-  return <BrowserRouter></BrowserRouter>
+  return <BrowserRouter>
+    <Routes>
+      <Route path="product" element={<Product />}/>
+    </Routes>
+  </BrowserRouter>
 }
 
 export default App 
