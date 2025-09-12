09ab00fb6076a1041836f5ba4ff7204259fd1ff2
karlijones <karlijones1984@gmail.com>
2025-09-11 20:44:08 -0500

    PageNotFound path added to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 
+"react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
-  return <BrowserRouter>
-    <Routes>
+  return (
+  <BrowserRouter>
+    <>
       <Route path="/" element={<Homepage />}/>
       <Route path="product" element={<Product />}/>
       <Route path="pricing" element={<Pricing />}/>
+      <Route path='*' element={<h1>PageNotFound</h1>}/>
     </Routes>
   </BrowserRouter>
+  );
 }
 
 export default App 
