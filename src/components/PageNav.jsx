efe22251b3f8ec105e79b216f09c5a052f4fa1c5
karlijones <karlijones1984@gmail.com>
2025-09-13 20:27:32 -0500

    Link to pricing and product added to PageNav.jsx

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -7,6 +7,12 @@ function PageNav() {
                 <li>
                     <Link to="/">Home</Link>
                 </li>
+                <li>
+                    <Link to="/pricing">Pricing</Link>
+                </li>
+                <li>
+                    <Link to="/product">Product</Link>
+                </li>
             </ul>
         </nav>
     )
