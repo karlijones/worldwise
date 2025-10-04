9b2ee38f6333c47e894e02ec23df0fdf93165cbf
karlijones <karlijones1984@gmail.com>
2025-10-04 14:28:32 -0500

    AppNav placeholder changed to Sidebar component

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,9 +1,10 @@
 import AppNav  from "../components/AppNav";
+import Sidebar from "../components/Sidebar";
 
 function AppLayout() {
     return (
         <div>
-            <AppNav />
+            <Sidebar />
         </div>
     );
 }
