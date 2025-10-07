31db6cd984c810b49ba0fd1fc6389cdfed5df83a
karlijones <karlijones1984@gmail.com>
2025-10-07 17:24:00 -0500

    css styles imported

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,6 +1,8 @@
 import AppNav  from "../components/AppNav";
 import Sidebar from "../components/Sidebar";
 
+import styles from "./AppLayout.module.css";
+
 function AppLayout() {
     return (
         <div>
