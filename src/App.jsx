424bae9f30d86e92d206c16ddefbd62205241a6e
karlijones <karlijones1984@gmail.com>
2025-10-08 20:28:17 -0500

    Nested Cities Route under AppLayout

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,9 @@ function App() {
       <Route path="product" element={<Product />}/>
       <Route path="pricing" element={<Pricing />}/>
       <Route path="login" element={<Login />}/>
-      <Route path="app" element={<AppLayout />}/>
+      <Route path="app" element={<AppLayout />}>
+        <Route path='cities' element={<p>List of Cities</p>}/>
+      </Route>
       <Route path="*" element={<PageNotFound />}/>
     </Routes >
   </BrowserRouter>
