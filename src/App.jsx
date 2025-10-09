64f0c6f9be1e839c41b8e2151637738403ffcbd1
karlijones <karlijones1984@gmail.com>
2025-10-08 20:31:17 -0500

    Nested route for form, cities, and countries under /app

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       <Route path="app" element={<AppLayout />}>
         <Route path='cities' element={<p>List of Cities</p>}/>
         <Route path='countries' element={<p>List of Countries</p>}/>
+        <Route path='form' element={<p>Form</p>}/>
       </Route>
       <Route path="*" element={<PageNotFound />}/>
     </Routes >
