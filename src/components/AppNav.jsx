b484c201610aa80198b1eaad96ee9e6b31d2dd5e
karlijones <karlijones1984@gmail.com>
2025-10-09 19:51:22 -0500

    ul added with links to Cities, Countries, and Form. started using NavLink for navigation.

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -2,7 +2,15 @@ import styles from './AppNav.module.css';
 
 function AppNav() {
     return (
-        <nav classname={styles.nav}>App navigation</nav>
+        <nav classname={styles.nav}>
+            <ul>
+                <li
+                <NavLink to='cities'>Cities</NavLink>
+                </li>
+                <li>Countries</li>
+                <li>Form</li>
+            </ul>
+        </nav>
     );
 }
 
