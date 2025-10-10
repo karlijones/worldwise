d7c937b3149bf0681b8290b9f6dd7485b61193af
karlijones <karlijones1984@gmail.com>
2025-10-09 20:20:36 -0500

    List of Cities changed to use CityList component

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       <Route path="app" element={<AppLayout />}>
         <Route index element={<CityList />} />
 
-        <Route path='cities' element={<p>List of Cities</p>}/>
+        <Route path='cities' element={<CityList />} />
         <Route path='countries' element={<p>List of Countries</p>}/>
         <Route path='form' element={<p>Form</p>}/>
       </Route>
