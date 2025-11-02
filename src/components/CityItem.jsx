e11dcf84687706bad25e6e3d4755f3d226bce283
karlijones <karlijones1984@gmail.com>
2025-11-02 11:26:38 -0600

    destructuring assignment to extract properties from the city object and apply CSS classes for styling.

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,9 +1,11 @@
 import styles from './CityItem.module.css';
 
 function CityItem({ city } ) {
+    const {cityName, emoji, date} = city;
+    
     return (
         <li className={styles.cityItem}
-        <span className={styles.emoji}></span>
+        <span className={styles.emoji}>{emjoi}</span>
         </li>
     );
 }
