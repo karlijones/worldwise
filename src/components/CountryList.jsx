9fcb6f67f56289747bebf3f5473ec51f0abf01c6
karlijones <karlijones1984@gmail.com>
2025-11-05 19:25:27 -0600

    changing country list component to correctly display list of countries

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,20 +1,20 @@
 import Spinner from './Spinner';
-import styles from './CountriesList.module.css';
-import CountriesItem from './CountriesItem';
+import styles from './CountryList.module.css';
+import CityItem from './CityItem';
 import Message from './Message';
 
 function CountryList({ cities, isLoading }) {
-    if{isLoading} return <Spinner />;
+    if {isLoading} return <Spinner />;
 
     if (!countries.length) return <Message message="Add your first city by clicking on a city on the map" />;
 
     return 
         <ul className={styles.countryList}>
-        {countries.map ({city} => (
+        {cities.map ({city} => (
             <CityItem city={city} key={city.id} />
         ))}
         </ul> 
     );
 }
 
-export default CityList;
+export default CountryList;
