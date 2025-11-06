c6875b01b43d344b872f5c51044a82fec540814f
karlijones <karlijones1984@gmail.com>
2025-11-05 19:36:04 -0600

    bug fix: Implement CountryList component to display list of countries

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -10,7 +10,7 @@ function CountryList({ cities, isLoading }) {
 
     const countries = [];
     
-    return 
+    return (
         <ul className={styles.countryList}>
         {countries.map ({country} => (
             <CountryItem country={country} key={country.id} />
