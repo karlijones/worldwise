19b3be47fffeb672e0de869c8f2b2f647450b01d
karlijones <karlijones1984@gmail.com>
2025-11-05 19:22:41 -0600

    countries changed to country

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -3,13 +3,13 @@ import styles from './CountriesList.module.css';
 import CountriesItem from './CountriesItem';
 import Message from './Message';
 
-function CountriesList({ cities, isLoading }) {
+function CountryList({ cities, isLoading }) {
     if{isLoading} return <Spinner />;
 
     if (!countries.length) return <Message message="Add your first city by clicking on a city on the map" />;
 
     return 
-        <ul className={styles.countriesList}>
+        <ul className={styles.countryList}>
         {countries.map ({city} => (
             <CityItem city={city} key={city.id} />
         ))}
