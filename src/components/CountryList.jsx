b9802af9fe347ab4cae5eb964e78ba9705b473dd
karlijones <karlijones1984@gmail.com>
2025-11-07 18:09:35 -0600

    replacing city with country

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -9,7 +9,7 @@ function CountryList({ cities, isLoading }) {
     if (!cities.length) return <Message message="Add your first city by clicking on a city on the map" />;
 
     const countries = cities.reduce((arr, city) => 
-        {if(arr.map(el=>el.city).includes(city.country)) 
+        {if(arr.map(el=>el.country).includes(city.country)) 
         return [...arr, {country: city.country, emoji: city.emoji}] 
         else return arr}
         , []);
