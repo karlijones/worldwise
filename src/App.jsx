33eb6d3f02ca7d5e293e59c6c4a58ce2fa848e63
karlijones <karlijones1984@gmail.com>
2025-11-08 18:29:11 -0600

    Cities Route with URL Parameter added

--- src/App.jsx
+++ src/App.jsx
@@ -49,6 +49,7 @@ function App() {
           element={<CityList  cities={cities} isLoading=
           {isLoading}/>} 
           />
+          <Route path='cities/:id' element={<City />}/>
         <Route path='countries' element={<CountryList cities={cities} isLoading={isLoading} />}/>
         <Route path='form' element={<p>Form</p>}/>
       </Route>
