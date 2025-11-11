ab6470d3cae7158772510d6debc3e5172d9f3e9d
karlijones <karlijones1984@gmail.com>
2025-11-11 15:43:14 -0600

    h6 and h3 tags to display the city name and its emoji.

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,7 +1,10 @@
 function City() {
     return (
         <div>
-            
+           <h6>City name</h6> 
+           <h3>
+            <span>{emoji}</span> {cityName}
+           </h3>
         </div>
     )
 }
