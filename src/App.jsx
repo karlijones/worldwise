994294d74ca928c7330258471b2a206f3803ad6b
karlijones <karlijones1984@gmail.com>
2025-11-17 20:20:41 -0600

    imported City component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
+import City from "./components/City";
 
 const BASE_URL = 'http://localhost:9000'
 
