372da6bc74765e57476cdb9dc782dbf7b32bfc14
karlijones <karlijones1984@gmail.com>
2025-11-20 19:46:26 -0600

    lat and lng query parameters to CityItem links

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,9 +3,15 @@ import styles from "./Map.module.css";
 
 function Map() {
     const [searchParams, setSearchParams] = useSearchParams();
+    const lat = searchParams.get("lat");
+    const lng = searchParams.get("lng");
 
     return (
-        <div className={styles.mapContainer}>Map 
+        <div className={styles.mapContainer}>
+            <h1>Map</h1>
+            <h1>
+                Position {lat}, {lng}
+            </h1>
         </div>
     );
 }
