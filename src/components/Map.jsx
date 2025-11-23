9994fab2a8ef2e9302d09fc6e7bce219a2bf20e7
karlijones <karlijones1984@gmail.com>
2025-11-23 13:51:49 -0600

    navigate function called to navigate to the form page when the map is clicked.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,12 +3,13 @@ import styles from "./Map.module.css";
 
 function Map() {
     const navigate = useNavigate();
+
     const [searchParams, setSearchParams] = useSearchParams();
     const lat = searchParams.get("lat");
     const lng = searchParams.get("lng");
 
     return (
-        <div className={styles.mapContainer} onClick={() => {}}>
+        <div className={styles.mapContainer} onClick={() => navigate("form")}>
             <h1>Map</h1>
             <h1>
                 Position {lat}, {lng}
