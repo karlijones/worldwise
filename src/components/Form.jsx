a307795c3b03991ff3c7f20204a23ac215f1d222
karlijones <karlijones1984@gmail.com>
2025-12-01 21:34:19 -0600

    textarea details added for notes input and state management

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,14 @@
+import { set } from "lodash";
+
 function Form() {
     return (
         <div>
             <label></label>
-            <textarea></textarea>
+            <textarea id="notes"
+            onChange={(e) => setNotes(e.target.value)}
+            value={notes}
+            >
+            </textarea>
         </div>
     )
 }
