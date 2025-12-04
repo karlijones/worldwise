a8b12b800fe659c224d8d13119aad57f80370f5b
karlijones <karlijones1984@gmail.com>
2025-12-03 21:46:55 -0600

    html notes added to label element in Form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,7 +3,8 @@ import { set } from "lodash";
 function Form() {
     return (
         <div classname={styles.row} >
-            <label></label>
+            <label htmlFor="notes">Notes about your trip to {City}
+            </label>
             <textarea id="notes"
             onChange={(e) => setNotes(e.target.value)}
             value={notes}
