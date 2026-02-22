d8315c58b2f669198e1e92364028bce7509f3637
karlijones <karlijones1984@gmail.com>
2026-02-22 17:08:31 -0600

    cleaning up import statements and adding a back button to the city page.

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,6 +1,9 @@
-import { useParams, useSearchParams } from "react-router";
-import styles from "./City.module.css";
+import { useEffect } from "react";
+import { useParams } from "react-router";
+import { useCities } from "../context/CitiesContext";
 import BackButton from "./BackButton";
+import styles from "./City.module.css";
+import Spinner from "./Spinner";
 
 const formatDate = (date =>
     new Intl.DateTimeFormat("en", {
