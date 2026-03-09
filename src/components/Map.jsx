473d823f07619f1fd3831ff5c6b4e093c62b5c6a
karlijones <karlijones1984@gmail.com>
2026-03-09 18:44:36 -0500

    button onClick added to Navigate to set search params for lat and lng.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -9,11 +9,14 @@ function Map() {
     const lng = searchParams.get("lng");
 
     return (
-        <div className={styles.mapContainer} onClick={() => navigate("form")}>
+        <div className={styles.mapContainer} onClick={() => 
+        navigate("form")}>
             <h1>Map</h1>
             <h1>
                 Position {lat}, {lng}
             </h1>
+            <button onClick={() => setSearchParams({ lat: 23, lng: 50 })}>
+            </button>
         </div>
     );
 }
