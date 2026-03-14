cba2ae75a614b688435b3398c41f6b94ee7c88ee
karlijones <karlijones1984@gmail.com>
2026-03-14 11:15:52 -0500

    deleting old map info to add new map info

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -11,11 +11,7 @@ function Map() {
     return (
         <div className={styles.mapContainer} onClick={() => 
         navigate("form")}>
-            <h1>Map</h1>
-            <h1>
-                Position {lat}, {lng}
-            </h1>
-            <button onClick={() => setSearchParams({ lat: 23, lng: 50 })}>
+            
                 Change pos
             </button>
         </div>
