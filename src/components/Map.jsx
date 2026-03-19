fde0757ccb074a4c75d95ed85eb27fdaa7a472ab
karlijones <karlijones1984@gmail.com>
2026-03-19 18:10:58 -0500

    MapContainer added to Map.jsx

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -11,7 +11,17 @@ function Map() {
     return (
         <div className={styles.mapContainer} onClick={() => 
         navigate("form")}>
-            
+            <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{ height: "100%", width: "100%" }}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={position}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
         </div>
     );
 }
