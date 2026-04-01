b330a4f1893e7bbcb1781dc2093d01ab3a3616c8
karlijones <karlijones1984@gmail.com>
2026-03-31 20:28:31 -0500

    position updated to mapPosition and search params added to get lat and lng from URL.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -5,21 +5,22 @@ import styles from "./Map.module.css";
 
 function Map() {
     const navigate = useNavigate();
-    const mapPosition = useState([40, 0]);
+    const [mapPosition, setMapPosition] = useState([40, 0]);
 
     const [searchParams, setSearchParams] = useSearchParams();
+
     const lat = searchParams.get("lat");
     const lng = searchParams.get("lng");
 
     return (
         <div className={styles.mapContainer} onClick={() => 
         navigate("form")}>
-            <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{ height: "100%", width: "100%" }}>
+            <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={false} style={{ height: "100%", width: "100%" }}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={position}>
+                <Marker position={mapPosition}>
                     <Popup>
                         A pretty CSS3 popup. <br /> Easily customizable.
                     </Popup>
