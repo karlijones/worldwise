cd51fc95c20ee36084ba3ab4feed508726b1b0b0
karlijones <karlijones1984@gmail.com>
2026-04-01 19:45:41 -0500

    temporarily removing event handler

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -13,9 +13,12 @@ function Map() {
     const lng = searchParams.get("lng");
 
     return (
-        <div className={styles.mapContainer} onClick={() => 
-        navigate("form")}>
-            <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={false} style={{ height: "100%", width: "100%" }}>
+        <div className={styles.mapContainer}
+            <MapContainer 
+            center={mapPosition} 
+            zoom={13} 
+            scrollWheelZoom={false} 
+            style={{ height: "100%", width: "100%" }}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
